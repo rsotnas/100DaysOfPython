ccdedd1f695b582927e6edb5f15c2a1abee92dae
felipe <fas.felipesantos@gmail.com>
2022-08-01 11:04:26 -0300

    .'

--- 3Day_TreasureIsland/main.py
+++ 3Day_TreasureIsland/main.py
@@ -25,6 +25,8 @@ print("Your mission is to find the treasure.")
 
 # https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
+# https://ascii.co.uk/art
+
 # Write your code below this line 👇
 action = input('Do you want to go Left or Right?')
 

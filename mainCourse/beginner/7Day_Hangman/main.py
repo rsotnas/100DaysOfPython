61b7bfd0ffee3501b7d90b47dda06ea3746bed29
felipe <fas.felipesantos@gmail.com>
2022-08-07 21:03:42 -0300

    .

---
+++ mainCourse/beginner/7Day_Hangman/main.py
@@ -0,0 +1,88 @@
+import random
+
+word_list = ['ardvark', 'babboon', 'camel']
+word = random.choice(word_list)
+display = []
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+for _ in word:
+    display.append('_')
+
+flag = True
+have_lives = 6
+print(''.join(display))
+while flag:
+    guess = input('Guess a letter: ').lower()
+    found_anything = False
+    for i in range(len(word)):
+        if word[i] == guess:
+            display[i] = guess
+            found_anything = True
+    if not found_anything:
+        have_lives -= 1
+        print(stages[have_lives])
+    print(''.join(display))
+
+    if '_' in display and have_lives:
+        continue
+    elif not have_lives:
+        flag = False
+        print('You lost.')
+    elif '_' not in display:
+        flag = False
+        print('You won.')
--- reboorgWorld/maze.py
+++ reboorgWorld/maze.py
@@ -1,25 +1,20 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
-import random
-
-
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
 
-def whole_movement(_move):
-    if front_is_clear():
-        move()
-    else:
-        if _move == 'left':
-            turn_left()
-        else:
-            turn_right()
-
+while front_is_clear():
+    move()
+turn_left()
 
 while not at_goal():
-    choices = ['left', 'right']
-    _move = random.choice(choices)
-    whole_movement(_move)
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
